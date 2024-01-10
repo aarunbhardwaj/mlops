0e5b4724e78d6f963bd0a050637227d9bd33faba
Aarun Bhardwaj <aarunbhardwaj@gmail.com>
2024-01-10 17:13:31 +0000

    added basic scaffolding

---
+++ hello.py
@@ -0,0 +1,8 @@
+def RamRam():
+    return "Ram Ram"
+
+
+def SSA():
+    # var= 5
+    # var=var
+    return "Sat Shree Akal"
---
+++ test_hello.py
@@ -0,0 +1,9 @@
+from hello import RamRam, SSA
+
+
+def test_RamRam():
+    assert "Ram Ram" == RamRam()
+
+
+def test_SSA():
+    assert "Sat Shree Akal" == SSA()
