bcf1b174e0fbda98b1ef3c899254480c12456e88
Aarun Bhardwaj <aarunbhardwaj@gmail.com>
2024-02-05 02:51:05 +0000

    added image, video and webcam

---
+++ webcam/video.py
@@ -0,0 +1,57 @@
+from ultralytics import YOLO
+import math
+import cv2
+
+capture = cv2.VideoCapture('../data/girls.mp4')
+
+frame_width = int(capture.get(3))
+frame_height = int(capture.get(4))
+
+# output = cv2.VideoWriter('output.mp4', cv2.VideoWriter.fourcc('M','J','P','Q'),10,(frame_width,frame_height))
+fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+
+output = cv2.VideoWriter('output.mp4', fourcc, 30, (frame_width, frame_height), True)
+
+model = YOLO('../weights/yolov8x.pt')
+classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
+              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
+              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
+              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
+              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
+              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+              "teddy bear", "hair drier", "toothbrush"
+              ]
+while True:
+    success, img = capture.read()
+    # Doing detections using YOLOv8 frame by frame
+    #stream = True will use the generator and it is more efficient than normal
+    results=model(img,stream=True)
+    #Once we have the results we can check for individual bounding boxes and see how well it performs
+    # Once we have have the results we will loop through them and we will have the bouning boxes for each of the result
+    # we will loop through each of the bouning box
+    for r in results:
+        boxes=r.boxes
+        for box in boxes:
+            x1,y1,x2,y2=box.xyxy[0]
+            #print(x1, y1, x2, y2)
+            x1,y1,x2,y2=int(x1), int(y1), int(x2), int(y2)
+            print(x1,y1,x2,y2)
+            cv2.rectangle(img, (x1,y1), (x2,y2), (255,0,255),3)
+            #print(box.conf[0])
+            conf=math.ceil((box.conf[0]*100))/100
+            cls=int(box.cls[0])
+            class_name=classNames[cls]
+            label=f'{class_name}{conf}'
+            t_size = cv2.getTextSize(label, 0, fontScale=1, thickness=2)[0]
+            #print(t_size)
+            c2 = x1 + t_size[0], y1 - t_size[1] - 3
+            cv2.rectangle(img, (x1,y1), c2, [255,0,255], -1, cv2.LINE_AA)  # filled
+            cv2.putText(img, label, (x1,y1-2),0, 1,[255,255,255], thickness=1,lineType=cv2.LINE_AA)
+    output.write(img)
+    cv2.imshow("Image", img)
+    if cv2.waitKey(1) & 0xFF==ord('1'):
+        break
+output.release()
